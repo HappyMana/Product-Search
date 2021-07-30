6d92c7a05f76b69ba1b8fb5725195d511bb2fcd7
HappyMana <c1002140@planet.kanazawa-it.ac.jp>
2021-07-30 19:58:46 +0900

    編集

--- resources/views/result.blade.php
+++ resources/views/result.blade.php
@@ -7,10 +7,10 @@
         @foreach($items as $key => $item)
             <div class="card col-3">
                 <a href="{{ route('search.result.detail', $key) }}">
-                    <img src="{{ $item->mediumImageUrl1 }}" class="card-img-top" alt="トップの画像" title="服の画像">
-                    <h5 class="card-title">{{ $item->itemName }}</h5>
-                    <p class="price">値段：{{ $item->itemPrice }}円</p>
-                    <p>ショップ：{{ $item->shopName }}</p>
+                    <img src="{{ $item["mediumImageUrl1"] }}" class="card-img-top" alt="トップの画像" title="服の画像">
+                    <h5 class="card-title">{{ $item["itemName"] }}</h5>
+                    <p class="price">値段：{{ $item["itemPrice"] }}円</p>
+                    <p>ショップ：{{ $item["shopName"] }}</p>
                 </a>
             </div>
         @endforeach
