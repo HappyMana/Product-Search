a6413e225812bcdbf16436bf2e9682388a47ee62
HappyMana <c1002140@planet.kanazawa-it.ac.jp>
2021-08-22 14:29:04 +0900

    タイトル変更

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>ショッピングサイト</title>
+    <title>商品検索サイト</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
